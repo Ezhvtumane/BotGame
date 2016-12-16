b9fa91218bc41af4fe502130d69c058563420359
GeorgyByCodenvy <gogaopel@gmail.com>
2016-12-16 14:09:02 +0000

    blank player & game classes part 2

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -1,13 +1,16 @@
 package bot;
 
+import bot.game.Player;
+
+/*
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-
+*/
 public class Main {
      public static void main(String[] args) {
 
-        ApiContextInitializer.init();
+    /*    ApiContextInitializer.init();
 
         TelegramBotsApi botsApi = new TelegramBotsApi();
 
@@ -17,5 +20,18 @@ public class Main {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }*/
+   
+    Player pl1 = new Player("1");
+    Player pl2 = new Player("2");
+    Player pl3 = new Player("3");
+    Player pl4 = new Player("4");
+    
+    System.out.println(
+        pl1.getNickName() + " : " + pl1.getID() + "; " + 
+        pl2.getNickName() + " : " + pl2.getID() + "; " + 
+        pl3.getNickName() + " : " + pl3.getID() + "; " + 
+        pl4.getNickName() + " : " + pl4.getID() + "; " );
     }
+
 }
--- src/main/java/bot/game/Player.java
+++ src/main/java/bot/game/Player.java
@@ -1,4 +1,27 @@
 package bot.game;
 
 public class Player {
+
+    private String     nickName;
+    private final int  ID;
+    private static int idCounter = 1;
+
+    public Player(String nickName) {
+        this.nickName = nickName;
+        this.ID = idCounter;
+        idCounter++;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    int getIdCounter() {
+        return idCounter;
+    }
+
 }
